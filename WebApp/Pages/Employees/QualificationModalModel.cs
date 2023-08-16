4f3ce7b890b9377df1f6ad060456958953de6784
Edgias Mudzukwa <mudzukwae@outlook.com>
2023-08-16 13:28:02 +0200

    Add qualification model

---
+++ WebApp/Pages/Employees/QualificationModalModel.cs
@@ -0,0 +1,28 @@
+﻿using Edgias.Humano.ApplicationCore.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Edgias.Humano.WebApp.Pages.Employees
+{
+    public class QualificationModalModel
+    {
+        [Required]
+        public string School { get; set; } = default!;
+
+        [Required]
+        public QualificationLevel Level { get; set; }
+
+        [Required]
+        public string FieldOfStudy { get; set; } = default!;
+
+        [Required]
+        public string Grade { get; set; } = default!;
+
+        [Required]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        public DateTime? EndDate { get; set; }
+
+        [Required]
+        public Guid EmployeeId { get; set; }
+    }
+}
