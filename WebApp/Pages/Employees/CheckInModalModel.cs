8780a7b879c8b95863983d262112e9d28313659c
Edgias Mudzukwa <mudzukwae@outlook.com>
2023-08-16 13:27:12 +0200

    Add check-in modal

---
+++ WebApp/Pages/Employees/CheckInModalModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Edgias.Humano.WebApp.Pages.Employees
+{
+    public class CheckInModalModel
+    {
+        [Required]
+        public Guid EmployeeId { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [Required]
+        public string TimeIn { get; set; } = default!;
+
+        public string? TimeOut { get; set; }
+    }
+}
