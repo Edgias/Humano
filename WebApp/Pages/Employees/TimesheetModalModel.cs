00cb05873369493a85ffcf0d714f6960b04a31e8
Edgias Mudzukwa <mudzukwae@outlook.com>
2023-08-16 13:26:40 +0200

    Add timesheet modal

---
+++ WebApp/Pages/Employees/TimesheetModalModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Edgias.Humano.WebApp.Pages.Employees
+{
+    public class TimesheetModalModel
+    {
+        [Required]
+        public string Description { get; set; } = default!;
+
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [Required]
+        public int NumberOfHours { get; set; }
+
+        [Required]
+        public Guid EmployeeId { get; set; }
+    }
+}
